626b14bf0115b71b004c601455395e94e58b4cad
Kemal-1 <kemal.zengin@hotmail.com>
2024-02-12 21:25:57 +0100

    Creating the User entity

---
+++ src/main/java/com/fitnesstracker/fitnesstracker/entity/User.java
@@ -0,0 +1,69 @@
+package com.fitnesstracker.fitnesstracker.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    // Define table fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    public User() {
+    };
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
