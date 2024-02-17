f0bcba870e6431fe6b45f8725ececdc3214ed05a
Kemal-1 <kemal.zengin@hotmail.com>
2024-02-17 23:04:51 +0100

    Adding RestController for Goal Entity

--- src/main/java/com/fitnesstracker/fitnesstracker/service/GoalServiceImpl.java
+++ src/main/java/com/fitnesstracker/fitnesstracker/service/GoalServiceImpl.java
@@ -3,10 +3,12 @@ package com.fitnesstracker.fitnesstracker.service;
 import com.fitnesstracker.fitnesstracker.dao.GoalRepository;
 import com.fitnesstracker.fitnesstracker.entity.Goal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class GoalServiceImpl implements GoalService {
 
     @Autowired
